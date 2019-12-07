05d75b823b58623c1fb8c6a10fe6dbfe5a012f9d
xiaoming <xiaoming@163.com>
2019-12-07 13:53:16 +0800

    这一次添加量第五次功能和red.js

--- red.js
+++ red.js
@@ -101,14 +101,3 @@
     </hashTree>
   </hashTree>
 </jmeterTestPlan>
-<dependency>
-<groupId>com.rabbitmq</groupId>
-<artifactId>amqp-client</artifactId>
-<version>4.0.3</version><!--此版本与spring boot 1.5.9版本匹配-->
-</dependency>
-<dependency>
-<groupId>org.springframework.boot</groupId>
-<artifactId>spring-boot-starter-logging</artifactId>
-</dependency>
-这是第四个功能
-这是第五个功能
